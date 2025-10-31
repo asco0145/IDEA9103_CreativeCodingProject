7ddaa426977185b9669ac6b11189c5bb557fa109
asco0145 <asco0145@uni.sydney.edu.au>
2025-10-31 17:35:45 +1100

    Starter Sketch File & Assets Update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,49 @@
+let baseImg, skyMask, waterMask, hillsMask, bridgeMask, guyMask;
+let sky, water, hills, bridge, guy;
+
+function preload(){
+  baseImg = loadImage("assets/scream.jpeg")
+  guyMask = loadImage("assets/guy.png")
+  skyMask = loadImage("assets/sky.png")
+  waterMask = loadImage("assets/water.png")
+  hillsMask = loadImage("assets/hills.png")
+  bridgeMask = loadImage("assets/bridge.png")
+
+  sky = new SkyArea(skyMask);
+  water = new WaterArea(waterMask);
+  hills = new HillsArea(hillsMask);
+  bridge = new BridgeArea(bridgeMask);
+  guy = new GuyArea(guyMask);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(baseImg.width, baseImg.height);
+
+  image(baseImg, 0, 0)
 }
 
 function draw() {
   background(220);
+
+  
+}
+
+class SkyArea {
+  
+}
+
+class WaterArea {
+  
+}
+
+class HillsArea {
+ 
+}
+
+class BridgeArea {
+  
+}
+
+class GuyArea {
+  
 }
