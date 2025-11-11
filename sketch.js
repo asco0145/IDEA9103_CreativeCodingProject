b3253350e439f5145aee4c5caf6fb2a63df98c74
asco0145 <asco0145@uni.sydney.edu.au>
2025-11-11 23:25:02 +1100

    Removing Test Code before starting Individual

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function setup() {
 
 createCanvas(baseImg.width, baseImg.height);
 
-hillsMask.resize(width, height); //Trying to resize mask
+hillsMask.resize(width, height);
 
 waterMask.resize(width, height);
 bridgeMask.resize(width, height);
@@ -130,53 +130,9 @@ point(x, y);
 
 }
 
-//Alex
-
 class HillsArea {
 
 constructor(maskImg){this.mask = maskImg;}
-
-/*drawLines() {
-
-//Draws 5 lines
-
-for (let i = 0; i < 5; i++){
-
-let x1 = random(width);
-
-let y1 = random(height);
-
-let x2 = random(width);
-
-let y2 = random(height);
-
-//Check Point 1
-
-let p1 = this.mask.get(int(x1), int(y1));
-
-let b1 = (p1[0] + p1[1] + p1[2]) /3; //greyscale
-
-//Check Point 2
-
-let p2 = this.mask.get(int(x2), int(y2));
-
-let b2 = (p2[0] + p2[1] + p2[2]) /3; //greyscale
-
-if(b1 < 200 || b2 < 200) continue; //avoid drawing in the black
-
-//Choses color for the painting
-
-let c = baseImg.get(int(x1), int(y1));
-
-strokeWeight(4);
-
-stroke(c[0], c[1], c[2], 180);
-
-line(x1, y1, x2, y2);
-
-}
-
-}*/
 
 drawPoints(){
 
