0be9b67932c593986746bfc9ee7463f224524193
selinay21 <zyan0171@uni.sydney.edu.au>
2025-11-11 14:54:10 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,6 +37,9 @@ hillsMask.resize(width, height); //Trying to resize mask
 waterMask.resize(width, height);
 bridgeMask.resize(width, height);
 
+skyMask.resize(width, height);
+
+
 //image(baseImg, 0, 0);
 
 }
@@ -49,12 +52,38 @@ hills.drawPoints();
 
 water.drawPoints();
 bridge.drawPoints();
+sky.drawStrokes();
 
 }
 
 class SkyArea {
+  constructor(maskImg){
+    this.mask = maskImg;
 
+  }
+drawStrokes() {
+  for (let y = 0; y < height; y += 6) { //loops through the y axis of the canvas in steps of 6 pixels
+
+    let offset = sin(radians(frameCount * 2 + y * 3)) * 10; // horizontal left right movement
+
+    for (let x = 0; x < width; x += 12) { //each iteration draws one short stroke, 10 pixels wide, along the row
+         // check if pixel belongs to sky (based on mask brightness)
+        let m = this.mask.get(x, y);
+        let bright = (m[0] + m[1] + m[2]) / 3;
+
+        if (bright > 40) {  // only draw strokes where mask is bright (sky area)
+      let c = baseImg.get(x, y); //use colours from base image
+      stroke(c[0], c[1], c[2], 200);
+      strokeWeight(3); // make each line 3 pixels thick
+
+      // wave movement per pixel
+      let yShift = sin((x * 0.5) + (frameCount * 0.005)) * 3; //vertical wave motion
+      line(x + offset, y + yShift, x + 10 + offset, y + yShift); // horizontal line
+    }
+  }}}
 }
+
+  
 
 class WaterArea {
 
