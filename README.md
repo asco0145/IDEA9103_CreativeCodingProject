ffdef937d43c74e0efc5b21793fde6a4c08cfc21
selinay21 <zyan0171@uni.sydney.edu.au>
2025-11-17 17:56:30 +1100

    Distortion of background over time changes

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # IDEA9103_CreativeCodingProject
-This is our final project. 
+
+# Instructions
+
+Load the page to see the animation start immediately and automatically. Move and scroll mouse down to reveal entire image. Click on the page if you want to lock in the full image. Move mouse horizontally to see more glitch and vertically for more wave. 
+
+# Details of your individual approach 
+
+I used Perlin noise for my individual coding part. My focus was wave distortion of the entire background image combined with particle-like jittering of the "guy" in the foreground, emphasizing fluid, natural distortion. Rather than using it as 5 different layers, I've used the original artwork as one layer and then the "guy" layer on top of that. The background painting is distorted and the foreground "guy" image moves smoothly with noise-based calculations in both X and Y, adding random jitters.
+
+# References 
+
+Similarly from Quiz 8, I was inspired by Refik Anadol's Machine Hallucinations series. This is a visual style characterised by fluid, constantly morphing, abstract forms and colours that seem to drift and shift like a liquid dream. It appears as a dynamic, continuous flow of visual information. The Perlin Noise is used to create that wavy motion.
+
+# Technical explanation
+
+The noise() function returns random numbers that can be tuned to feel organic. I found this from the p5.js reference website: https://p5js.org/reference/#/p5/noise. This is extremely relevant as it generates the Perlin values. Other functions that we haven't covered in the Perlin Noise lecture are map() which re-maps a number from one range to another. These were used in order to have a growing Perlin noise distortion effect that evolves smoothly, and retains the original painting's structure while transforming it dynamically over time.
+
+
+
+The foreground guy image position is controlled using sine waves jittery movement. The random() function add unpredictability and irregular bursts, providing natural "twitches."
+The code achieves this by looping over every row of pixels in screamImg and using copy() to shift the pixel rows horizontally according to Perlin noise.
+The guy is drawn last to overlay the background dynamically.
+
+## Changes to group code
+
+I've made a lot of changes to the group code and have pretty much made my  Instead of the different 5 layers that we had in the group code, I chose to use the original artwork as the background. The group made the effects of the background slowly appear using dot art. Mine is more distorted and glitchy. Instead of revealing using the dot art, mine shows the effects straight away. 
+
--- individual.js
+++ individual.js
@@ -1,10 +1,12 @@
 let screamImg, guyImg;
 let w, h;
 let scaleFactor = 0.4;
+let revealLocked = false; 
+
 
 function preload() {
-  screamImg = loadImage('TheScream.png');   // painting
-  guyImg = loadImage('TheScreamGuy.png');   // foreground guy layer
+  screamImg = loadImage('assets/TheScream.png');   // painting
+  guyImg = loadImage('assets/TheScreamGuy.png');   // foreground guy layer
 }
 
 function setup() {
@@ -18,18 +20,64 @@ function setup() {
 function draw() {
   background(255);
 
-  // mouse position controls Perlin parameters
-  let speed = map(mouseX, 0, width, 0.005, 0.03);      // horizontal movement = animation speed
-  let magnitude = map(mouseY, 0, height, 5, 30);       // vertical movement = wave magnitude
+  // gradually increase distortion over time
+ //let magnitude = map(frameCount, 0.5, 2000, 20, 200); //map() re-maps a number from one range to another
+// magnitude = constrain(magnitude, 15, 80); //constrain() constrains a number between a minimum and maximum value
 
+
+// let speed = 0.04; // animation speed of wave
+
+
+ // animate background with Perlin noise based wave distortion
+// for (let y = 0; y < h; y++) {
+  //let offset = noise(frameCount * speed, y * 0.05) * magnitude - magnitude / 2; // noise() is from p5js.org/reference/#/p5/noise
+   //let jitter = random(-1, 1); // random small jitter
+   //if (random(1) < 0.01) jitter += random(-10, 10); //  large jitter
+
+
+   //copy(screamImg, 0, y, w, 1, offset + jitter, y, w, 1); // copy() copies pixels from a source image to a region of the canvas
+ //}
+
+ 
+  // mouse position controls Perlin parameters 
+  //map() re-maps a number from one range to another
+  let speed = map(mouseX, 0, width, 0.005, 0.03); // horizontal movement = animation speed 
+  let magnitude = map(mouseY, 0, height, 5, 30); // vertical movement = wave magnitude 
+  // // top to bottom reveal 
+  // let revealHeight = map(mouseY, 0, height, 0, h); 
+
+
+  let revealHeight;
+  if (revealLocked) {
+    revealHeight = h;         // full reveal
+  } else {
+    revealHeight = map(mouseY, 0, height, 0, h);  // interactive reveal
+  }
+
+  // animate only up to 'revealHeight' 
+  for (let y = 0; y < revealHeight; y++) { 
+    // mouse moves change all offsets for more interaction 
+    let offset = noise(frameCount * speed, y * 0.04 + mouseX * 0.01) * magnitude - magnitude / 2; // noise() is from p5js.org/reference/#/p5/noise
+    copy(screamImg, 0, y, w, 1, offset, y, w, 1); } // copy() to shift the pixel rows horizontally
+
+ 
  
   // animate the guy image with random values
 
- let guyYOffset = sin(frameCount * speed * 2 + mouseY) * magnitude * 0.4;
-  let guyXOffset = noise(frameCount * speed * 2 + mouseX) * magnitude * 0.5 - magnitude * 0.25;
+ let guyYOffset = sin(frameCount * speed * 2) * magnitude * 0.4;
+  let guyXOffset = noise(frameCount * speed * 2) * magnitude * 0.5 - magnitude * 0.25; 
+   let jitterX = random(-25, 25);
+let jitterY = random(-25, 25);
 
+image(guyImg, guyXOffset + jitterX, guyYOffset + jitterY);
 
-image(guyImg, guyXOffset, guyYOffset);
   }
+
+  // click to see full image
+  function mousePressed() {
+  revealLocked = true;
+}
+
+
 
 
